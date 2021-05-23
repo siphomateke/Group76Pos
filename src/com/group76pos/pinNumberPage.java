a624b937422ce113dbd1ad73ada2343814f64d64
justin <oj.brndt@gmail.com>
2021-05-23 13:59:12 +0200

    pinNumberPage UI

--- src/com/group76pos/pinNumberPage.java
+++ src/com/group76pos/pinNumberPage.java
@@ -1,4 +1,10 @@
 package com.group76pos;
 
+import javax.swing.*;
+
 public class pinNumberPage {
+    private JPanel mainPanel;
+    private JPanel panelTwo;
+    private JButton OKButton;
+    private JTextField pinNumTextField;
 }
