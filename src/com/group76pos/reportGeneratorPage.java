77f4c267aba1f6105ad2bbcb5ba2fe6a0b963a23
justin <oj.brndt@gmail.com>
2021-05-23 14:26:08 +0200

    pinNumberPage UI

---
+++ src/com/group76pos/reportGeneratorPage.java
@@ -0,0 +1,13 @@
+package com.group76pos;
+
+import javax.swing.*;
+
+public class reportGeneratorPage {
+    private JPanel mainPanel;
+    private JPanel panelTwo;
+    private JRadioButton byItemsRadioButton;
+    private JRadioButton byCustomersRadioButton;
+    private JRadioButton byTransfersRadioButton;
+    private JRadioButton byMonthRadioButton;
+    private JButton generateReportButton;
+}
