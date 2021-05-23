5aef929ae24d12cca1abb2ef529ddb4532541a62
justin <oj.brndt@gmail.com>
2021-05-23 13:39:37 +0200

    accountNumberPage UI

---
+++ src/com/group76pos/accountNumberPage.java
@@ -0,0 +1,10 @@
+package com.group76pos;
+
+import javax.swing.*;
+
+public class accountNumberPage {
+    private JPanel mainPanel;
+    private JTextField accNumTextField;
+    private JButton OKButton;
+    private JPanel pannelTwo;
+}
