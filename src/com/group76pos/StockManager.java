8703d5bc0570352af0d23fe98fb8221519b2d2bd
siphomateke <siphomateke@gmail.com>
2021-05-23 13:42:09 +0200

    Implement StockManager saving and restoring

--- src/com/group76pos/StockManager.java
+++ src/com/group76pos/StockManager.java
@@ -1,12 +1,24 @@
 package com.group76pos;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StockManager implements IMemento {
   public static StockManager instance;
   ArrayList<Product> products;
 
-  private StockManager() {}
+  private StockManager() {
+    this.products = new ArrayList<>(Arrays.asList(
+      new Product(1, "Chicken Wrap with Mayo", 10, 60, 84, 5),
+      new Burger(2, "Veggie Burger", 50, 40, 65, 45, false, true),
+      new Fries(3, "Large Chips", 60, 10, 20, 50, ProductSize.Large),
+      new Product(4, "Salad", 5, 5, 15, 0),
+      new Drink(5, "Small Coke Can", 70, 4, 8, 50, ProductSize.Small, false),
+      new Drink(6, "Large Coke", 50, 10, 16, 35, ProductSize.Large, true)
+    ));
+  }
 
   public static StockManager getInstance() {
     return instance;
@@ -24,12 +36,21 @@ public class StockManager implements IMemento {
 
   @Override
   public Memento save() {
-    // TODO: Auto-generated method stub
-    return null;
+    Gson gson = new Gson();
+    String jsonString = gson.toJson(this.products);
+    return new Memento(jsonString);
   }
 
   @Override
   public void restore(Memento m) {
-    // TODO: Auto-generated method stub
+    Gson gson = new Gson();
+    Product[] restoredProducts = gson.fromJson(m.state, Product[].class);
+    for (Product p: restoredProducts) {
+      for (int i = 0; i < this.products.size(); i++) {
+        if (p.id == this.products.get(i).id) {
+          this.products.set(i, p);
+        }
+      }
+    }
   }
 }
