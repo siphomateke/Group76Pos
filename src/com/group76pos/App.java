8756ae455d0896a79b7630d17c2ca30e154b6e53
siphomateke <siphomateke@gmail.com>
2021-05-23 12:55:06 +0200

    add ui (account)

--- src/com/group76pos/App.java
+++ src/com/group76pos/App.java
@@ -1,7 +1,21 @@
 package com.group76pos;
+import javax.swing.*;
 
-public class App {
-  public static void main(String[] args) {
-    
+public class App extends JFrame {
+    private JPanel mainPanel;
+    private JTextField accNumTextField;
+    private JLabel accNumLabel;
+    private JButton accNumOKButton;
+
+    public App(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame Frame = new App("Group76 POS");
+        Frame.setVisible(true);
   }
 }
