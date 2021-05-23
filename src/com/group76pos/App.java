b49c3fe472dad4da58e1eb4c3de392df1c4f55f7
justin <oj.brndt@gmail.com>
2021-05-23 13:19:57 +0200

    mainPage UI (for ordering)

--- src/com/group76pos/App.java
+++ src/com/group76pos/App.java
@@ -3,14 +3,25 @@ import javax.swing.*;
 
 public class App extends JFrame {
     private JPanel mainPanel;
+
+    // the food buttons
     private JButton burgersButton;
     private JButton friesButton;
     private JButton drinksButton;
+
+    // report button should generate report somehow (maybe on a new menu)
     private JButton reportsButton;
+
+    // cancel button should quit the program or something
     private JButton cancelButton;
     private JButton checkoutButton;
+
+    // this should show the items that are added to the order/cart
     private JList listOrder;
     private JLabel totalAmountLabel;
+
+    // this should be able to change the items dynamically based on the food buttons
+    private JList foodList;
 
     public App(String title) {
         super(title);
